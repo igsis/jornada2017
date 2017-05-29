2ebee11d091bd1ab234a6dbbe524cd5f09bb9c61
Grayce Ferreira <grayce.ferreira@gmail.com>
2017-05-29 15:04:20 -0300

    Conteúdo

---
+++ wp-config-db-sample.php
@@ -0,0 +1,29 @@
+<?php
+
+/** O nome do banco de dados do WordPress */
+define('DB_NAME', 'jornada_virada');
+
+/** Usuário do banco de dados MySQL */
+define('DB_USER', 'jornada_virada');
+
+/** Senha do banco de dados MySQL */
+define('DB_PASSWORD', 'abacaxi123@');
+
+/** nome do host do MySQL */
+define('DB_HOST', 'jornada_virada.mysql.dbaas.com.br');
+
+/** Conjunto de caracteres do banco de dados a ser usado na criação das tabelas. */
+define('DB_CHARSET', 'utf8');
+
+/** O tipo de collate do banco de dados. Não altere isso se tiver dúvidas. */
+define('DB_COLLATE', '');
+
+define('MINHA_VIRADA__DB_NAME', DB_NAME);
+define('MINHA_VIRADA__USER', DB_USER);
+define('MINHA_VIRADA__PASS', DB_PASSWORD);
+define('MINHA_VIRADA__HOST', DB_HOST);
+
+define('NAS_REDES__DB_NAME', DB_NAME);
+define('NAS_REDES__USER', DB_USER);
+define('NAS_REDES__PASS', DB_PASSWORD);
+define('NAS_REDES__HOST', DB_HOST);
